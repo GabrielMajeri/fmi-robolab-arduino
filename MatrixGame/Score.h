b95e44595c6be67f624ade2e1ceabfc90272171c
Gabriel Majeri <gabriel.majeri6@gmail.com>
2019-12-19 15:16:34 +0200

    Refactor Score class

--- MatrixGame/Score.h
+++ MatrixGame/Score.h
@@ -1,37 +1,23 @@
 #pragma once
 
-#include "Settings.h"
-
-static const int giftPoints = 15;
-static const int heightPoints = 5;
-
-static const int easyPointMultiplier = 1;
-static const int mediumPointMultiplier = 2;
-static const int hardPointMultiplier = 3;
-
 using ScoreT = unsigned;
 
 class Score {
   ScoreT current = 0;
 
  public:
-  ScoreT setCurrentScore(ScoreT newScore) { current = newScore; }
-  ScoreT getCurrentScore() const { return current; }
+  void setCurrentScore(ScoreT newScore);
+  ScoreT getCurrentScore() const;
 
-  ScoreT getScoreMultiplier() {
-    switch (settings.difficulty) {
-      case Difficulty::Easy:
-        return easyPointMultiplier;
-      case Difficulty::Medium:
-        return mediumPointMultiplier;
-      case Difficulty::Hard:
-        return hardPointMultiplier;
-    }
-  }
+  void reset();
 
-  void addPointsForGift() { current += giftPoints * getScoreMultiplier(); }
+  void addPointsForGift();
+  void addPointsForHeight();
+  void addPointsForTimeLeft(byte secondsLeft);
+  void addPointsForLivesLeft(byte livesLeft);
 
-  void addPointsForHeight() { current += heightPoints * getScoreMultiplier(); }
+  bool isHighScore() const;
+  void updateHighScoreList();
 };
 
 extern Score score;
--- MatrixGame/Score.ino
+++ MatrixGame/Score.ino
@@ -1,3 +1,53 @@
 #include "Score.h"
 
+#include "Persistent.h"
+#include "Settings.h"
+#include "Utility.h"
+
+static const int giftPoints = 15;
+static const int heightPoints = 5;
+static const int extraLifePoints = 50;
+
 Score score;
+
+void Score::setCurrentScore(ScoreT newScore) { current = newScore; }
+
+ScoreT Score::getCurrentScore() const { return current; }
+
+void Score::reset() { current = 0; }
+
+void Score::addPointsForGift() {
+  current += giftPoints * getScoreMultiplierByDifficulty();
+}
+
+void Score::addPointsForHeight() {
+  current += heightPoints * getScoreMultiplierByDifficulty();
+}
+
+void Score::addPointsForTimeLeft(byte secondsLeft) {
+  current += secondsLeft * getScoreMultiplierByDifficulty();
+}
+
+void Score::addPointsForLivesLeft(byte extraLives) {
+  current += extraLives * extraLifePoints * getScoreMultiplierByDifficulty();
+}
+
+bool Score::isHighScore() const {
+  const SavedScore& lastPlayer = persistentData.topPlayers[maxHighScores - 1];
+  return current >= lastPlayer.score;
+}
+
+void Score::updateHighScoreList() {
+  SavedScore& lastPlayer = persistentData.topPlayers[maxHighScores - 1];
+
+  strncpy(lastPlayer.name, playerName, playerNameLen);
+  lastPlayer.score = current;
+
+  for (int i = maxHighScores - 2; i >= 0; --i) {
+    if (persistentData.topPlayers[i].score <
+        persistentData.topPlayers[i + 1].score) {
+      swap(persistentData.topPlayers[i], persistentData.topPlayers[i + 1]);
+    }
+  }
+  savePersistentData();
+}
